0256b5a07fce18ad825a3857a1fa12a25b86f987
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-09-05 12:43:54 -0500

    completed project

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace RandomClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var random = new Random();
+
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength;i++)
+               buffer[i] = (char)('a' + random.Next(0, 26)); //create random words using char casting on random numbers
+            
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+
+            //Console.WriteLine((int)'a');  //return character code for 'a'
         }
     }
 }
